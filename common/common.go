a4ea5f4275da5ebabbabf3e1a56b139322841a9c
iloveicedgreentea <31193909+iloveicedgreentea@users.noreply.github.com>
2021-12-27 17:54:54 -0500

    common update

--- common/common.go
+++ common/common.go
@@ -2,6 +2,9 @@ package common
 
 import (
 	log "github.com/sirupsen/logrus"
+	"os"
+	"bufio"
+	"strconv"
 )
 
 // checks an error 
@@ -9,4 +12,46 @@ func CheckErr(e error) {
 	if e != nil {
 		log.Fatal(e)
 	}
+}
+
+func ReadFile(fileName string) ([]string) {
+	dataFile, err := os.Open(fileName)
+	CheckErr(err)
+
+	defer dataFile.Close()
+
+	// create scanner to read file
+	scanner := bufio.NewScanner(dataFile)
+
+	// store the lines as string
+	var lines []string
+	
+	// for each line, read and add it to string array
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines
+}
+
+func ReadFileInt(fileName string) ([]int) {
+	dataFile, err := os.Open(fileName)
+	CheckErr(err)
+
+	defer dataFile.Close()
+
+	// create scanner to read file
+	scanner := bufio.NewScanner(dataFile)
+
+	// store the lines as string
+	var lines []int
+
+	// for each line, read and add it to int array
+	for scanner.Scan() {
+		convLine, err := strconv.Atoi(scanner.Text())
+		CheckErr(err)
+		lines = append(lines, convLine)
+	}
+
+	return lines
 }
